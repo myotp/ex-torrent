2b06805884b0f3ae2e61790ff5e35fd93645f7b4
jia.wang <wj2016@gmail.com>
2023-11-29 11:25:15 +0100

    first commit

---
+++ lib/ex_torrent.ex
@@ -0,0 +1,18 @@
+defmodule ExTorrent do
+  @moduledoc """
+  Documentation for `ExTorrent`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTorrent.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_torrent_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTorrentTest do
+  use ExUnit.Case
+  doctest ExTorrent
+
+  test "greets the world" do
+    assert ExTorrent.hello() == :world
+  end
+end
